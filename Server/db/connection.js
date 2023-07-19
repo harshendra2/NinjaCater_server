75dda66751dbcbf0ac631ea79165a61a83be7731
harshendra raj <harsendraraj20@gmail.com>
2023-07-19 18:53:05 +0530

    Ninja catering database connectiopn update

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const DB =  process.env.DATABASE;
+const DB = process.env.DATABASE;
 
-mongoose.connect(DB,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-}).then(()=>console.log("Data base connected"))
-.catch((error)=>{
-    console.log("error",error);
+mongoose.connect("mongodb+srv://harshendra:Narayana20Ha@cluster0.misnwgb.mongodb.net/CaterNinja", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 })
+  .then(() => console.log("Database connected"))
+  .catch((error) => {
+    console.log("Error:", error);
+  });
